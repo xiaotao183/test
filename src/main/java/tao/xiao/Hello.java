678640f11c636cee62a45ba19f2ba592a2fc6970
tao.xiao <xiaotao183@gmail.com>
2011-04-14 00:11:10 +1000

    new file

---
+++ src/main/java/tao/xiao/Hello.java
@@ -0,0 +1,14 @@
+package tao.xiao;
+
+public class Hello {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
