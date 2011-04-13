37855dab86a1987ec6b3192baca8a9a0f9b3f086
tao.xiao <xiaotao183@gmail.com>
2011-04-13 23:57:26 +1000

    first commit

---
+++ src/main/java/tao/xiao/App.java
@@ -0,0 +1,13 @@
+package tao.xiao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
