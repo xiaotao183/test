3b131e24c6832e0105b2aa4f26aa87022ad4825f
tao.xiao <xiaotao183@gmail.com>
2011-05-02 22:25:12 +1000

    testtest

--- src/main/java/tao/xiao/App.java
+++ src/main/java/tao/xiao/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(new Hello("hello world!!"));
+        System.out.println(new Hello("hello world!!!"));
     }
 }
