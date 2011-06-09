e0cd7fa59da7381c5b27eb17ac1b9273f4254266
xiaotao183 <xiaotao183@gmail.com>
2011-06-09 11:23:27 +1000

    tweak

--- src/main/java/tao/xiao/BinaryHeap.java
+++ src/main/java/tao/xiao/BinaryHeap.java
@@ -1,72 +1,110 @@
 package tao.xiao;
 
+import org.apache.commons.lang.ArrayUtils;
+
 public class BinaryHeap {
-	private int[] heap;
+    private int[] heap;
+    private int heapSize;
 
-	public BinaryHeap() {
-	}
+    public BinaryHeap() {
+    }
 
-	public void buildHeap(int[] elements) {
-		if (elements != null) {
-			heap = new int[elements.length + 1];
-			for (int i = 0; i < elements.length; i++) {
-				heap[i + 1] = elements[i];
-			}
+    public void buildHeap(final int[] elements) {
+        if (elements != null) {
+            heap = new int[elements.length + 1];
+            heapSize = heap.length - 1;
+            heap[0] = Integer.MAX_VALUE;
 
-			for (int i = elements.length / 2; i > 0; i--) {
-				percolateDown(i);
-			}
-		}
-	}
+            for (int i = 0; i < elements.length; i++) {
+                heap[i + 1] = elements[i];
+            }
 
-	private void percolateDown(int index) {
-		if (getLeftChildIndex(index) < heap.length) {
-			int maxChildIndex = getMaxChildIndex(index);
+            for (int i = elements.length / 2; i > 0; i--) {
+                percolateDown(i);
+            }
+        }
+    }
 
-			if (heap[index] < heap[maxChildIndex]) {
-				swap(index, maxChildIndex, heap);
-				percolateDown(maxChildIndex);
-			}
-		}
-	}
+    private void percolateDown(final int index) {
+        if (getLeftChildIndex(index) <= heapSize) {
+            int maxChildIndex = getMaxChildIndex(index);
 
-	private int getMaxChildIndex(int parentIndex) {
-		if (getRightChildIndex(parentIndex) < heap.length) {
-			if (heap[getLeftChildIndex(parentIndex)] > heap[getRightChildIndex(parentIndex)]) {
-				return getLeftChildIndex(parentIndex);
-			} else {
-				return getRightChildIndex(parentIndex);
-			}
-		} else {
-			return getLeftChildIndex(parentIndex);
-		}
-	}
+            if (heap[index] < heap[maxChildIndex]) {
+                swap(index, maxChildIndex, heap);
+                percolateDown(maxChildIndex);
+            }
+        }
+    }
 
-	private int getLeftChildIndex(int parentIndex) {
-		return 2 * parentIndex;
-	}
+    private int getMaxChildIndex(final int parentIndex) {
+        if (getRightChildIndex(parentIndex) <= heapSize) {
+            if (heap[getLeftChildIndex(parentIndex)] > heap[getRightChildIndex(parentIndex)]) {
+                return getLeftChildIndex(parentIndex);
+            } else {
+                return getRightChildIndex(parentIndex);
+            }
+        } else {
+            return getLeftChildIndex(parentIndex);
+        }
+    }
 
-	private int getRightChildIndex(int parentIndex) {
-		return 2 * parentIndex + 1;
-	}
+    private int getLeftChildIndex(final int parentIndex) {
+        return 2 * parentIndex;
+    }
 
-	private void swap(int from, int to, int[] array) {
-		int tmp = array[to];
-		array[to] = array[from];
-		array[from] = tmp;
-	}
+    private int getRightChildIndex(final int parentIndex) {
+        return 2 * parentIndex + 1;
+    }
 
-	public void print() {
-		if (heap != null) {
-			for (int i = 1; i < heap.length; i++) {
-				System.out.print(heap[i]);
-			}
-		}
-	}
+    private void swap(final int from, final int to, final int[] array) {
+        int tmp = array[to];
+        array[to] = array[from];
+        array[from] = tmp;
+    }
 
-	public static void main(String[] args) {
-		BinaryHeap bh = new BinaryHeap();
-		bh.buildHeap(new int[] { 4, 5, 1, 6, 9, 9, 2, 7, 8, 3 });
-		bh.print();
-	}
+    public void insert(final int element) {
+        if (!ArrayUtils.isEmpty(heap)) {
+            if (heapSize < heap.length - 1) {
+                int i = ++heapSize;
+                for (; heap[i / 2] < element; i /= 2) {
+                    heap[i] = heap[i / 2];
+                }
+
+                heap[i] = element;
+            }
+        }
+    }
+
+    public int deleteMax() {
+        if (!ArrayUtils.isEmpty(heap)) {
+            int max = heap[1];
+            swap(1, heapSize--, heap);
+            percolateDown(1);
+
+            return max;
+        }
+
+        return Integer.MAX_VALUE;
+    }
+
+    public void print() {
+        if (heap != null) {
+            for (int i = 1; i <= heapSize; i++) {
+                System.out.print(heap[i] + ",");
+            }
+        }
+    }
+
+    public static void main(final String[] args) {
+        BinaryHeap bh = new BinaryHeap();
+        bh.buildHeap(new int[] { 4, 5, 1, 6, 9, 9, 2, 7, 8, 3, 11, 10 });
+        bh.print();
+        System.out.println();
+        System.out.println(bh.deleteMax());
+        bh.print();
+        System.out.println();
+        
+        bh.insert(200);
+        bh.print();
+    }
 }
