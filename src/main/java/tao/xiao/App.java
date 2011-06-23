73f98a583f6ba9a6accb6b6a84c109f995dbee56
xiaotao183 <xiaotao183@gmail.com>
2011-06-23 16:05:19 +1000

    test egit 1.0

--- src/main/java/tao/xiao/App.java
+++ src/main/java/tao/xiao/App.java
@@ -6,8 +6,8 @@ package tao.xiao;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( final String[] args )
     {
-        System.out.println(new Hello("hello world!!!"));
+        System.out.println(new Hello("hello world!!!!!"));
     }
 }
