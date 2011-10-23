78fe3e47905f6d25beda1aae8c34653da62eeac4
tao.xiao <xiaotao183@gmail.com>
2011-10-23 11:37:45 +1100

    more changes

--- src/main/java/tao/xiao/annotations/AnnotatedClass.java
+++ src/main/java/tao/xiao/annotations/AnnotatedClass.java
@@ -2,10 +2,13 @@ package tao.xiao.annotations;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AnnotatedClass {
+import org.apache.commons.collections.CollectionUtils;
 
-	@EaiFieldName(value = "named")
+public class AnnotatedClass implements AnnotatedInterface {
+
 	public String getName() {
 		return "tt";
 	}
@@ -14,13 +17,21 @@ public class AnnotatedClass {
 			IllegalAccessException, InvocationTargetException {
 		AnnotatedClass clazz = new AnnotatedClass();
 
-		Method[] ms = clazz.getClass().getMethods();
+		System.out.println("ddd");
+		List<Class<?>> clazzs = new ArrayList<Class<?>>();
+		CollectionUtils.addAll(clazzs, clazz.getClass().getInterfaces());
+		clazzs.add(clazz.getClass().getSuperclass());
+		clazzs.add(clazz.getClass());
 
-		for (Method m : ms) {
-			if (m.isAnnotationPresent(EaiFieldName.class)) {
-				EaiFieldName e = m.getAnnotation(EaiFieldName.class);
-				System.out.println(e.value());
-				System.out.println(m.invoke(clazz));
+		for (Class<?> cla : clazzs) {
+			Method[] ms = cla.getMethods();
+
+			for (Method m : ms) {
+				if (m.isAnnotationPresent(EaiFieldName.class)) {
+					EaiFieldName e = m.getAnnotation(EaiFieldName.class);
+					System.out.println(e.value());
+					System.out.println(m.invoke(clazz));
+				}
 			}
 		}
 	}
